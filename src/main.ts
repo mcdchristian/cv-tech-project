f8753952c3ac06d01bcafa7f572f6702e6620836
mcdchristian <mcdchristian4@gmail.com>
2026-02-26 15:45:38 +0200

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+    origin: ['http://localhost:4200'],
+  };
+  app.enableCors(corsOptions);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
-      // whitelist: true,
-      // forbidNonWhitelisted: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     }),
   );
   await app.listen(process.env.PORT ?? 3000);

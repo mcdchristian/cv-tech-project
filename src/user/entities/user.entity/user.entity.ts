3399bdbe35eded7b2ac89ba84f567c0e2837eb9e
mcdchristian <mcdchristian4@gmail.com>
2026-03-04 14:26:45 +0200

    fix-authentication register/login

--- src/user/entities/user.entity/user.entity.ts
+++ src/user/entities/user.entity/user.entity.ts
@@ -1,22 +1,32 @@
 import { CvEntity } from '../../../cv/entities/cv.entity/cv.entity';
+import { UserRoleEnum } from '../../../enums/user-role.enum';
 import { TimestampEntities } from './../../../generics/timestamp.entities';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
 export class UserEntity extends TimestampEntities {
   @PrimaryGeneratedColumn()
-  id?: number;
+  id!: number;
 
   @Column({ nullable: true, unique: true, length: 50 })
-  name?: string;
+  username!: string;
 
   @Column({ nullable: true, unique: true, length: 100 })
-  email?: string;
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  salt!: string;
+
+  @Column({ enum: UserRoleEnum, type: 'enum', default: UserRoleEnum.USER })
+  role!: string;
 
   @OneToMany(() => CvEntity, (cv) => cv.user, {
     cascade: true,
     eager: true,
     nullable: true,
   })
-  cvs?: CvEntity[];
+  cvs!: CvEntity[];
 }
